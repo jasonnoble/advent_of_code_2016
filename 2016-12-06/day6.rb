e214e76aedf8dcf0a8150303bfad8408fd2a5e63
Jason Noble <github+jasonn@jasonnoble.org>
2016-12-06 09:51:29 -0700

    Day 6

---
+++ 2016-12-06/day6.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+# messages = <<END_OF_MESSAGES
+# eedadn
+# drvtee
+# eandsr
+# raavrd
+# atevrs
+# tsrnev
+# sdttsa
+# rasrtv
+# nssdts
+# ntnada
+# svetve
+# tesnvt
+# vntsnd
+# vrdear
+# dvrsen
+# enarar
+# END_OF_MESSAGES
+
+messages = File.read('day6.input')
+
+@counts = []
+messages.split("\n").map do |message|
+  message.chars.each_with_index do |letter, index|
+    @counts[index] ||= Hash.new(0)
+    @counts[index][letter] += 1
+  end
+end
+
+print "Message: "
+@counts.each do |count|
+  print count.sort_by{|_,v| v}.reverse.first.first
+end
+
+puts ''
+
+@counts.each_with_index do |count, index|
+  puts "#{index}: #{count.sort_by{|_,v| v}.reverse.inspect}"
+end
